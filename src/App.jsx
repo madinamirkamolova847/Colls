07c69883b76503b9e231d7df9117d2339fe24b75
Soliha <salihamirkamalova07@gmail.com>
2026-03-02 15:40:29 +0500

    Added Cold page

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,15 @@ import React from 'react'
 import Navbar from './components/Navbar'
 import { Route, Routes } from 'react-router-dom'
 import Product from './pages/Product'
+import Cold from './pages/Cold'
 
 const App = () => {
   return (
     <div>
       <Navbar/>
       <Routes>
-        <Route path='/prod' element={<Product/>}/>
+        <Route path='/coffee' element={<Product/>}/>
+        <Route path='/cold' element={<Cold/>}/>
       </Routes>
     </div>
   )
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div>
+        <h1>Information about drinks</h1>
+       
+        <Link to={"/coffee"}>
+            <h3>Coffee</h3>
+        </Link>
+        
+        <Link to={"/cold"}>
+             <h3>Cold drinks</h3>
+        </Link>
+       
+    
+    </div>
   )
 }
 
