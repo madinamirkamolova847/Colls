9351a2e316a9acc53a99b688817ba1e45705070e
Salimxadjayeva Komila <salimxadjayevakomila@Salimxadjayevas-MacBook-Air.local>
2026-03-02 16:42:52 +0500

    cold pages

--- src/pages/Cold.jsx
+++ src/pages/Cold.jsx
@@ -1,11 +1,26 @@
-import React from 'react'
-
-const Cold = () => {
-  return (
-    <div>
-        
-    </div>
-  )
-}
-
-export default Cold
+ import React from 'react'
+ 
+ const Cold = () => {
+   return (
+     <div>
+      <div className='drink'>Cold drinks</div>
+      <h2>MIlk sheik</h2>
+      <div className='coffee'>
+        <img src="https://i.pinimg.com/736x/29/81/71/298171317e8a03757eef5cd758dca102.jpg" alt="x" />
+        <p> milk sheik — это популярный кофейный напиток, состоящий из одного или двух шотов эспрессо, разбавленных горячей водой (обычно в соотношении от 1:1 до 1:4).</p>
+      </div>
+      <h2>Mojito</h2>
+      <div className='coffee'>
+        <img src="https://i.pinimg.com/736x/bd/6f/bc/bd6fbc9c90d2657490afb1ddc97c7c28.jpg" alt="x" />
+        <p>Mojito — это популярный кофейный напиток родом из Италиисостоящий из одной-двух порций эспрессо (1/3) и большого количества парового молока (2/3) с тонким слоем (около 1 см) нежнойвоздушной пены.</p>
+      </div>
+      <h2>Strawberry juice</h2>
+        <div className='coffee'>
+        <img src="https://i.pinimg.com/736x/75/4d/85/754d85dba68eaf9b61c137bb6c15eab6.jpg" alt="x" />
+        <p>Strawberry juice — это концентрированный кофейный напиток (25–35 мл) с плотной золотистой пенкой (крема́)получаемый путем пропускания горячей воды (92–95°C) под давлением около 9 бар через спрессованный мелкомолотый кофе</p>
+      </div>
+     </div>
+   )
+ }
+ 
+ export default Cold
